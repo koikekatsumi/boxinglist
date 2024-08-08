70daf66b1f6d9fcdbffb6ac0f8b757d714bf326b
koikekatsumi <katsumikoike724@gmail.com>
2024-08-08 18:42:57 +0900

    プロジェクトの初期設定

---
+++ src/main/java/com/example/boxinglist/BoxinglistApplication.java
@@ -0,0 +1,13 @@
+package com.example.boxinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxinglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxinglistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boxinglist/BoxinglistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boxinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxinglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
